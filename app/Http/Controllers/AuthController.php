dd653fa7d70164d52b5cf3c181df80f4c03361cc
Anyaso Franklin <franko172000@gmail.com>
2022-08-19 02:05:09 +0100

    Modified logout method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Actions\AuthenticationAction;
+use App\Actions\GetFollowedStreamsAction;
 use App\Http\Resources\AuthResource;
 use App\Services\Twitch\TwitchOAuthClient;
 
@@ -20,6 +21,12 @@ class AuthController extends Controller
     {
         $user = AuthenticationAction::run([
             'code' => request()->input('code')
+        ]);
+
+        //This is can be run in the background UserStreamsJob::class job
+        // Running this synchronously because the test server does not have que worker available
+        GetFollowedStreamsAction::run([
+            'user' => $user,
         ]);
 
         $token = $user->createToken('access_token')->plainTextToken;
